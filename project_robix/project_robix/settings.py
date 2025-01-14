35e6bb64b21e21617a2a96fc3cb7fe3e6bb10247
nijiya-curions <nijiya.curions@gmail.com>
2025-01-14 20:59:13 +0530

    changes in our settings.py

--- project_robix/project_robix/settings.py
+++ project_robix/project_robix/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR /"robixapp"/ "static"]
 
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
